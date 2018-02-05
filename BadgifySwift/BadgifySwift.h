83a60226f605c14dbb469ab690b36eba2adc194e
maimai <gigi10991@gmail.com>
2018-02-05 19:17:18 +0200

    Add .gitignore

---
+++ BadgifySwift/BadgifySwift.h
@@ -0,0 +1,19 @@
+//
+//  BadgifySwift.h
+//  BadgifySwift
+//
+//  Created by Mai Mai on 2/5/18.
+//  Copyright © 2018 maimaios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BadgifySwift.
+FOUNDATION_EXPORT double BadgifySwiftVersionNumber;
+
+//! Project version string for BadgifySwift.
+FOUNDATION_EXPORT const unsigned char BadgifySwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BadgifySwift/PublicHeader.h>
+
+
